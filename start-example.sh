05ba8b381bb64ef1058727d88ca4a4e62916bb7b
Shu Wu <shuwu83@gmail.com>
2016-04-19 17:59:09 -0700

    some GMaps integration

--- start-example.sh
+++ start-example.sh
@@ -6,6 +6,8 @@ export WIT_TOKEN=your_access_token
 export FB_PAGE_ID=your_page_id
 export FB_PAGE_TOKEN=your_page_token
 export FB_VERIFY_TOKEN=any_token
+export GMAPS_KEY=your_key
+export ONE_BUS_AWAY_KEY=your_key
 
 node messenger.js
 
--- start-interactive-sample.sh
+++ start-interactive-sample.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export IS_INTERACTIVE=1
-export WIT_TOKEN=YOUR_WIT_TOKEN
+export WIT_TOKEN=your_access_token
+export GMAPS_KEY=your_key
+export ONE_BUS_AWAY_KEY=your_key
 
 node interactive.js
-
